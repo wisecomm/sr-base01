d783ef68963f129ac26e1df0490d20f1e1256349
danyoh <danyoh#gmail.com>
2025-12-26 16:56:40 +0900

    remove

--- sb-base-04/src/main/java/com/example/demo/common/exception/GErrorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface GErrorInterface {
-    public HttpStatus getStatus();
-    public String getCode();
-    public String getMessage();
-}
--- sb-base-04/src/main/resources/db/seed/local/V20250402125602__Initial_Seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `BS_USER` (`USER_ID`, `USER_NAME`, `PASSWORD`, `EMAIL`, `PHONE`, `ENABLED`, `LAST_LOGIN_AT`, `CREATED_AT`, `UPDATED_AT`)
-VALUES
-	('danyoh', '홍길동1', '1234', NULL, NULL, 1, NULL, current_timestamp(), current_timestamp());
